7f2c722b7c76e9afb75c7d873b5b9f58a1879373
Beykel Daniel <cabrerajosedaniel89@gmail.com>
2026-05-18 14:31:32 +0100

    usuarios

--- resources/views/actividades/partials/lista.blade.php
+++ resources/views/actividades/partials/lista.blade.php
@@ -24,7 +24,10 @@
         <div class="mt-auto flex justify-between items-end">
             <div class="flex flex-col gap-1.5">
                 <span class="text-[12px] text-blue-500 font-black uppercase">Cupos: {{ $a->cupos }}</span>
-                <button data-actividad='@json($a)' class="btn-ver-inscritos text-[10px] text-indigo-600 font-bold uppercase hover:text-indigo-800 hover:bg-indigo-100 transition-colors bg-indigo-50 px-3 py-1 rounded w-fit border border-indigo-100 shadow-sm flex items-center gap-1">
+                
+                {{-- Modificado para incluir la relación de creador (user) y la de inscritos (users) --}}
+                <button data-actividad='@json(array_merge($a->toArray(), ["user" => $a->user, "users" => $a->users]))' 
+                        class="btn-ver-inscritos text-[10px] text-indigo-600 font-bold uppercase hover:text-indigo-800 hover:bg-indigo-100 transition-colors bg-indigo-50 px-3 py-1 rounded w-fit border border-indigo-100 shadow-sm flex items-center gap-1">
                     <i class="fa-solid fa-users"></i> Ver inscritos
                 </button>
             </div>
@@ -34,8 +37,9 @@
                     ¡Apuntado!
                 </button>
             @else
+                {{-- Modificado para incluir la relación de creador (user) y la de inscritos (users) --}}
                 <button 
-                    data-actividad='@json($a)'
+                    data-actividad='@json(array_merge($a->toArray(), ["user" => $a->user, "users" => $a->users]))'
                     class="btn-ver-mas-act bg-[#82aeb4] text-white px-4 py-1.5 rounded-lg font-black text-[10px] uppercase hover:bg-[#32424D] transition-colors shadow-md">
                     Ver más
                 </button>
